4c64fe63d1f26a38d4b1d4b52eff119dfdb21a38
nwitherell <nwitherellwork@gmail.com>
2023-04-22 20:34:02 -0400

    SvelteKit Original Coomit

---
+++ SaySeeSvelteKit/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import MainPageCarousel from './components/MainPage/mainPageCarousel.svelte';
+	import MainPageImageGrid from './components/MainPage/mainPageImageGrid.svelte';
+	import MainPageLatestImage from './components/MainPage/mainPageLatestImage.svelte';
+</script>
+
+<main>
+	<div class="container">
+		<MainPageCarousel />
+
+		<MainPageLatestImage />
+		<MainPageImageGrid />
+	</div>
+</main>
+
+<style>
+	main {
+		font-family: sans-serif;
+		text-align: center;
+	}
+
+	.container {
+		display: grid;
+		grid-template-rows: repeat(3, 33% [row-start]);
+	}
+</style>
